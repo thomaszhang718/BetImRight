f899fbb4d82723f4c13cf11d7ac4e070834482b6
John Grant Bunner <scribe@exposed2tce.com>
2016-09-29 19:02:13 -0400

    betImRight

---
+++ models/bets.js
@@ -0,0 +1,26 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+	selectAll: function(callback) {
+		orm.selectAll('products', function(res){
+			callback(res);
+		});
+	},
+	insertOne:  function(cols, vals, callback) {
+		orm.insertOne('products', cols, vals, function(res){
+			callback(res);
+		});
+	},
+	update: function(objColVals, condition, callback) {
+		orm.updateOne('products', objColVals, condition, function(res){
+			callback(res);
+		});
+	},
+	auth: function(username, callback) {
+		orm.updateOne('users', username, function(res){
+			callback(res);
+		});
+	}
+};
+
+module.exports = burger;
---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var jwt  = require('jsonwebtoken');
+
+var app = express();
+
+app.set('jwtSecret', "CODINGROCKS");
+
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Routes
+var routes = require('./controllers/burger_controller.js');
+app.use('/', routes);
+
+// Starts the server to begin listening
+var PORT = 8080;
+app.listen(PORT, function(){
+	console.log('App listening on: http://localhost:' + PORT);
+})
+
+
