a59c1860a33416edeced949f1012daa51e59d707
Thomas Zhang <thomaszhang718@gmail.com>
2016-10-14 21:45:12 -0400

    Changed database query string for user auth

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-/*var mysql = require('mysql');
+var mysql = require('mysql');
 
 var connection;
 
@@ -21,44 +21,6 @@ else {
         database: 'bets_db'
     });
 }
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
-module.exports = connection;*/
-
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
-
-var mysql = require('mysql');
-
-var source = {
-
-    localhost: {
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: "123456t",
-        database: "burgers_db"
-    },
-
-    jawsDB: {
-        port: 3306,
-        host: 'uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'f6fzr3rzzsl2z76f',
-        password: "vi2o9a4qmbk6xs3k",
-        database: "xt8w3b7ov2wm8tua"
-    }
-}
-
-var connection = mysql.createConnection(source.jawsDB);
-
 
 connection.connect(function(err) {
     if (err) {
--- config/orm.js
+++ config/orm.js
@@ -65,7 +65,9 @@ var orm = {
     },
 	
 	userAuth: function(table, callback) {
-        var queryString = 'SELECT `' + table + '`.`user_id`,`' + table + '`.`username`,`' + table + '`.`password`, `' + table + '`.`admin` FROM `bets_db`.`' + table + '`;';
+        var queryString = 'SELECT * FROM ' + table + ";";
+        console.log(queryString);
+
         connection.query(queryString, function(err, result) {
             if (err) throw err;
             callback(result);
